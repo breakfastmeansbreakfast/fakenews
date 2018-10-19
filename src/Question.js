004455df98cab063a1ae30bb3a190f0b1884be52
breakfastmeansbreakfast <77744c@gmail.com>
2018-10-20 00:34:20 +0100

    friday update
    - added new dependency for react-bootstrap
    - added new dependency for express
    - static version of the question module is now rendering

---
+++ src/Question.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './App.css';
+import {Radio} from 'react-bootstrap'
+const headline = "Test headline!";
+
+class Question extends Component {
+  render() {
+    return (
+      <div className="questionArea">
+      <h1>{headline}</h1>
+      <form>
+      <Radio name="radioGroup" inline>
+        Fake
+      </Radio>{' '}
+      <Radio name="radioGroup" inline>
+        Legit
+      </Radio>{' '}
+      </form>      
+      </div>
+    )
+  }
+}
+
+export default Question;
