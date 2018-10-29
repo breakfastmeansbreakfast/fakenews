22e6d565d912fca4ebe133b408544232a3b76efb
Linda <linda.tamakaha@gmail.com>
2018-10-29 14:42:58 +0000

    first commit

--- src/About.js
+++ src/About.js
@@ -1,16 +1,41 @@
 import React, { Component } from 'react';
 import './App.css';
-import {Alert} from 'react-bootstrap'
-const title = "About us!";
+import {Alert, Grid, Row, Col, Thumbnail, Image} from 'react-bootstrap';
+
+
+const title = "The creators of the fake news App!";
 
 class About extends Component {
+
   render() {
     return (
-      <div className="Learnarea">
+    <div className="Learnarea">
       <h1>{title}</h1>
-      <Alert>Linda to do!</Alert>
-      </div>
-    )
+      <Alert>Creative Team</Alert>
+      <Grid>
+  <Row>
+    <Col xs={6} md={4}>
+      <Thumbnail src="/thumbnaildiv.png" alt="242x200">
+        <h3>Thumbnail label</h3>
+        <p>Description</p>
+      </Thumbnail>
+    </Col>
+    <Col xs={6} md={4}>
+      <Thumbnail src="/thumbnaildiv.png" alt="242x200">
+        <h3>Thumbnail label</h3>
+        <p>Description</p>
+      </Thumbnail>
+    </Col>
+    <Col xs={6} md={4}>
+      <Thumbnail src="/baliLinda.png" alt="242x200">
+        <h3>Thumbnail label</h3>
+        <p>Description</p>
+      </Thumbnail>
+    </Col>
+  </Row>
+</Grid>;
+</div>
+    );
   }
 }
 
--- src/Learn.js
+++ src/Learn.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
-import {Alert} from 'react-bootstrap'
-const title = "How to spot fake news!";
+import {Alert} from 'react-bootstrap';
+const title = "Fake News";
 
 class Learn extends Component {
   render() {
     return (
       <div className="Learnarea">
       <h1>{title}</h1>
-      <Alert>Linda to do!</Alert>
+      <Alert>What is Fake news</Alert>
+      <p>"Fake news" was not a term many people used two years ago, but it is now seen as one of the greatest threats to democracy, free debate and the Western order.
+      <br/>As well as being a favourite term of Donald Trump, it was also named 2017's word of the year, raising tensions between nations, and may lead to regulation of social media.
+      <br/>And yet, nobody can agree on what it is, the extent of the problem, and what to do about it. Here's everything you need to know.</p>
+      <br/>
+      <h2>The origins of fake news</h2>
+      <br/>
+      <p>Governments and powerful individuals have used information as a weapon for millennia, to boost their support and quash dissidence.  
+      <br/>
+      Octavian famously used a campaign of disinformation to aid his victory over Marc Anthony in the final war of the Roman Republic.<br/>
+      In its aftermath, he changed his name to Augustus, and dispatched a flattering and youthful image of himself throughout the Empire, maintaining its use in his old age.
+      <br/>
+      In the 20th century, new forms of mass communication allowed propaganda's scale and persuasive power to grow, particularly during wartime and in fascist regimes.
+      <br/>This sort of propaganda was largely funded and controlled by governments, but the blatant bias it carried waned as the ideological struggles became less apparent.<br/>Added to that, as populations became more used to mass communication, they could more easily see through it.  
+      </p>
+      <h3>HOw to spot it! </h3>
+      
       </div>
     )
   }
