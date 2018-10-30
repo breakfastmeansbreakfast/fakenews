0c6bbfd9730531a7de82b39412929e9902bd2a87
breakfastmeansbreakfast <77744c@gmail.com>
2018-10-30 20:21:46 +0000

    toms changes

--- src/Question.js
+++ src/Question.js
@@ -3,6 +3,30 @@ import './App.css';
 import {Radio} from 'react-bootstrap'
 import axios from 'axios';
 
+headlines = {
+  headline1: {
+    headlineID: 1,
+    headline: "The first headline",
+    leftOrRight: "L",
+    source: "www.bbc.co.uk",
+    descripion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi quis finibus justo. Sed tincidunt arcu nunc, ac egestas nulla pellentesque pretium. Donec id pretium justo. Ut vitae felis eget mi blandit pretium. In vel consequat risus. Aenean nec sodales nisl, non rhoncus velit. Integer faucibus massa vitae gravida porta. Donec nec ornare nulla. Nullam condimentum semper magna, et sagittis massa.",
+  },
+  headline2: {
+    headlineID: 2,
+    headline: "The second headline",
+    leftOrRight: "R",
+    source: "www.lbc.co.uk",
+    descripion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi quis finibus justo. Sed tincidunt arcu nunc, ac egestas nulla pellentesque pretium. Donec id pretium justo. Ut vitae felis eget mi blandit pretium. In vel consequat risus. Aenean nec sodales nisl, non rhoncus velit. Integer faucibus massa vitae gravida porta. Donec nec ornare nulla. Nullam condimentum semper magna, et sagittis massa.",
+  },
+  headline3: {
+    headlineID: 3,
+    headline: "The third headline",
+    leftOrRight: "L",
+    source: "www.bumface.co.uk",
+    descripion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi quis finibus justo. Sed tincidunt arcu nunc, ac egestas nulla pellentesque pretium. Donec id pretium justo. Ut vitae felis eget mi blandit pretium. In vel consequat risus. Aenean nec sodales nisl, non rhoncus velit. Integer faucibus massa vitae gravida porta. Donec nec ornare nulla. Nullam condimentum semper magna, et sagittis massa.",
+  }
+}
+
 //let headline = "Test headline!";
 
 /*axios.get(`http://localhost:9999/question`)
