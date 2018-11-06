249fd71ebbecb11f969bf3e5ea6535d8935662de
breakfastmeansbreakfast <77744c@gmail.com>
2018-11-06 00:31:39 +0000

    added improved test feedback

--- src/Question.js
+++ src/Question.js
@@ -1,23 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import {Alert, Radio, Button} from 'react-bootstrap';
-// import axios from 'axios';
 
-/*axios.get(`http://localhost:9999/question`)
-  .then(res => {
-    const question = res.data;
-    this.setState({ question });
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .then(function () {
-    // always executed
-  });
-
-console.log(this.state.question);
-*/
 class Question extends Component {
   state = {
     currentQuestionIndex: 0,
@@ -27,13 +11,13 @@ class Question extends Component {
     headlines: [
      {
         headlineID: 0,
-        headline: "Trump once compared undocumented immigrants to animals.",
-        answer: "legit",
+        headline: "Trump ramps up rhetoric on undocumented immigrants: 'These aren't people. These are animals.'",
+        answer: "fake",
         leftOrRight: "L",
         source: "Reported by USA Today",
         sourceurl: "",
-        proofurl: null,
-        explainer: "Yes, Trump did say this word for word. He said, 'These aren't people. These are animals'",
+        proofurl: "https://www.bbc.co.uk/news/av/world-us-canada-44148697/trump-immigrant-gangs-animals-not-people",
+        explainer: "Surprise! If you listen to his speach, Trump was not comparing all immegrants to animals, only the ones in 'problem' gangs.",
         descripion: "",
       },
       {
@@ -43,16 +27,18 @@ class Question extends Component {
         leftOrRight: "R",        
         source: "Theresa May as Prime Minister (2018)",
         sourceurl: null,
+        explainer: "This has been widely refuted and is now considered by many to be misleading.",
         proofurl: "https://www.ft.com/content/845f9e56-cd4c-11e8-9fe5-24ad351828ab",
         descripion: "[....] No more sending vast sums of money each year to the EU – instead a Brexit dividend to spend on domestic priorities like our long-term plan for the NHS.",
       },
       {
         headlineID: 2,
-        headline: "Australia is moving further north",
+        headline: "Australia is moving further north, meaning many will have to adjust their sat navs in the future.",
         answer: "legit",
-        leftOrRight: "N",        
+        leftOrRight: "N",
         source: "A guy in the pub",
         sourceurl: null,
+        explainer: "Umm, yep, technically the contenant of Austrialia is moving further north so this is true.",
         proofurl: "https://www.bbc.co.uk/cbbc/quizzes/real-or-fake-news-quiz",
         descripion: "",
       },
@@ -63,31 +49,45 @@ class Question extends Component {
         leftOrRight: "R",        
         source: "www.dailypresser.com",
         sourceurl: null,
+        explainer: "As Trump would say, NEVER HAPPENED, OK. Seriously, despite this fake, it did not stop the story from potentially being seen by thousands of users on Facebook.",
         proofurl: "https://davidbuckingham.net/2017/01/12/fake-news-is-media-literacy-the-answer/",
         descripion: "",
       },
       {
         headlineID: 4,
+        headline: "German Chancellor Angela Merkel calls for German-Led EU army",
+        answer: "fake",
+        leftOrRight: "R",        
+        source: "Daily Express",
+        sourceurl: "https://www.express.co.uk/news/world/753750/Angela-Merkel-Germany-EU-army-Trump-Brexit",
+        explainer: "Fake! This was 'corrected' by The Express after many months, the orignal story that claimed that German Chancellor Angela Merkel was calling for an EU army to defend Europe had no real substantiation for the claims but nonetheless was picked up by several other news outlets as well as social media users.",
+        proofurl: "https://inews.co.uk/opinion/top-ten-fake-misleading-news-stories-2017/",
+        descripion: "ANGELA Merkel has renewed her call for the European Union to have its own army, warning the bloc will not be able to rely on others to guarantee its security with Donald Trump in the White House and Britain set to leave.",
+      },
+      {
+        headlineID: 5,
         headline: "Trump wanted to ban all muslims from the US",
         answer: "fake",
         leftOrRight: "R",        
         source: "Panel show guest on TV",
         sourceurl: null,
-        proofurl: "https://davidbuckingham.net/2017/01/12/fake-news-is-media-literacy-the-answer/",
-        descripion: "",
-      },
-      {
-        headlineID: 5,
-        headline: "Leaving the EU will generate £350m a week for Britain, which could be spent towards the NHS",
-        answer: "fake",
-        leftOrRight: "R",        
-        source: "Leave.eu",
-        sourceurl: null,
+        explainer: "Trump's widely condemned 'Muslim Ban' was an interesting one. Technically he did not 'ban all muslims' as reported, instead banning countries which had a high Muslim population. However some will say that he was deliberatly vague, in order to fit in with the ideals of his far right core support.",
         proofurl: "https://davidbuckingham.net/2017/01/12/fake-news-is-media-literacy-the-answer/",
         descripion: "",
       },
       {
         headlineID: 6,
+        headline: "Leaving the EU will generate £350m a week for Britain, which could be spent towards the NHS",
+        answer: "fake",
+        leftOrRight: "R",        
+        source: "Leave.eu, and on the side of a Bus",
+        sourceurl: null,
+        explainer: "FullFact (the independant Fact Checking charity) says this best: This is wrong, it’s more like £250 million a week. In any case the impact on the economy from changes to trade after leaving the EU is likely to be far bigger than savings from the UK’s membership fee.",
+        proofurl: "https://fullfact.org/europe/350-million-week-boris-johnson-statistics-authority-misuse/",
+        descripion: "",
+      },
+      {
+        headlineID: 7,
         headline: "Influx of immigrants costs every UK household £350 a year",
         answer: "fake",
         leftOrRight: "R",
@@ -98,22 +98,13 @@ class Question extends Component {
         descripion: "Labour's 'open door' policy on immigration costs every household £350 a year, it was claimed yesterday. David Coleman, an Oxford University academic, puts the total annual bill to the taxpayer at almost £8.8billion. In a submission to a House of Lords committee, he said there had been an 'absent-minded commitment' to increase the population by one million every five years...",
       },
       {
-        headlineID: 7,
-        headline: "German Chancellor Angela Merkel calls for German-Led EU army",
-        answer: "fake",
-        leftOrRight: "R",        
-        source: "Daily Express",
-        sourceurl: "https://www.express.co.uk/news/world/753750/Angela-Merkel-Germany-EU-army-Trump-Brexit",
-        proofurl: "https://inews.co.uk/opinion/top-ten-fake-misleading-news-stories-2017/",
-        descripion: "ANGELA Merkel has renewed her call for the European Union to have its own army, warning the bloc will not be able to rely on others to guarantee its security with Donald Trump in the White House and Britain set to leave.",
-      },
-      {
         headlineID: 8,
         headline: "Sadiq Khan gets reprimanded by the Queen for his comments on Trump",
         answer: "fake",
         leftOrRight: "R",
         source: "Multiple US pulications",
         sourceurl: null,
+        explainer: "FAKE. The Queen has to remain impartial on items like this, and this was no exception. Arguably Trump's comments and lies about Sadqiq were more derogatory.",
         proofurl: "https://inews.co.uk/opinion/top-ten-fake-misleading-news-stories-2017/",
         descripion: "Sadiq Khan had been reprimanded by the Queen for his comments and that the UK media were in support of his telling off.",
       },
@@ -159,16 +150,6 @@ class Question extends Component {
       },
     ],
   }
-
-  // componentDidMount() {
-  //   axios.get(`http://localhost:9999/question`)
-  //     .then(res => {
-  //       const question = res.data;
-  //       this.setState({ question });        
-  //       headline = this.state.question.map(question => question.headline)
-  //       console.log(headline)
-  //     })
-  // }
 
   handleChange = (changeEvent) => {
     this.setState({
@@ -225,7 +206,7 @@ class Question extends Component {
     let responseArray = this.state.responses
     let headlineArray = this.state.headlines
     let score = 0;
-    for (var i = 0; i < headlineArray.length; ++i) {
+    for (var i = 0; i < headlineArray.length; ++i) {// eslint-disable-next-line
       if (responseArray[i].answer == headlineArray[i].answer) {
         score +=1
       }
@@ -249,22 +230,15 @@ class Question extends Component {
       useralert = <Alert bsStyle='success'><strong>You got it correct!</strong></Alert>
     }}
 
-    let explainer = () => {if (this.state.answerCheck === false) {
-      console.log(this.state.headlines[this.state.currentQuestionIndex].explainer)
-      explainer = <p></p>
-    } else {
-      explainer = <p>{this.state.headlines[this.state.currentQuestionIndex].explainer}</p>
-    }}
-
     if (!this.state.clearCheck) {
       useralert()
    }
 
     return (
       <div className="questionArea">
-      <h1>{currentquestion.headline}</h1>
-      <strong>{currentquestion.source}</strong>
-      <p>{currentquestion.descripion}</p>
+      <h1 className="divheadline">{currentquestion.headline}</h1>
+      <h4 className="divsource">Source: {currentquestion.source}</h4> <br />
+      <p>{currentquestion.descripion}</p> 
       <form>
       <Radio className="radiobuttonfake" value="fake" checked={this.state.selectedAnswer === 'fake'} onChange={this.handleChange} inline>
         Fake
@@ -274,8 +248,13 @@ class Question extends Component {
       </Radio>{''}
       <br /> <br /> <br />
       {useralert}<br />
+      <p>
+      {!this.state.clearCheck ? <h3>Explainer:</h3> : null } 
+      {!this.state.clearCheck ? this.state.headlines[this.state.currentQuestionIndex].explainer : null } <br />
+      {!this.state.clearCheck ? this.state.headlines[this.state.currentQuestionIndex].proofurl : null } <br /> <br />
+      </p>
       {button}<br />
-      <strong>{!this.state.clearCheck ? this.state.headlines[this.state.currentQuestionIndex].explainer : <p></p> }</strong>
+
       </form><br />
       </div>
     )
--- src/Results.js
+++ src/Results.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class Question extends Component {
+class Results extends Component {
   render() {
     return (
     <div className="Learnarea">
       <h1>Results</h1>
-      <p>{this.props.location.state.message}</p>
+      <h2>{this.props.location.state.message}</h2>
+      <p>Nice work completing the test. Please learn more about Fake News and how to spot it by clicking the Learn tab above.</p>
       </div>
 );
 }}
-//https://davidbuckingham.net/2017/01/12/fake-news-is-media-literacy-the-answer/ <add in
-export default Question;
+
+export default Results;
