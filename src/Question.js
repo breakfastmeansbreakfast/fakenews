18ad954e0f212f15c36cd64fea5c1a9885fab1c0
breakfastmeansbreakfast <77744c@gmail.com>
2018-11-06 00:48:01 +0000

    bug fix for react router

--- src/Question.js
+++ src/Question.js
@@ -196,7 +196,7 @@ class Question extends Component {
     if (this.state.currentQuestionIndex === this.state.headlines.length -1) {
       this.savetoHistory()
       let score = this.getScore()
-      this.props.history.push({pathname: '/results', state: {score: score, message: `Well.. your results are in! You scored ${score} out of 9.`, responses: this.state.responses, headlines: this.state.headlines}});
+      this.props.history.push({pathname: '/results', state: {score: score, message: `Well.. your results are in! You scored ${score} out of 9.`}});
     } else {
     this.savetoHistory()
     this.setState({currentQuestionIndex: this.state.currentQuestionIndex +1})
